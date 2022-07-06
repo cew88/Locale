9c7cd4f346c63a442ab0be958a26f96e21042173
cew88 <cew8@rice.edu>
2022-07-05 17:51:55 -0700

    Create pop-up for when users visit a location on their list of places to visit; uses Lottie library (external UI library)

---
+++ app/src/main/java/com/example/locale/fragments/LocationVisitedFragment.java
@@ -0,0 +1,32 @@
+package com.example.locale.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.locale.R;
+
+public class LocationVisitedFragment extends DialogFragment {
+
+    public LocationVisitedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getDialog().setTitle("visited dialog");
+        return inflater.inflate(R.layout.fragment_location_visited, container, false);
+    }
+}
