09a9d000cdc4babbd82087dedd1c22f5bbe93f4f
cew88 <cew8@rice.edu>
2022-07-26 10:39:45 -0700

    Updated reset password activity and layout

--- app/src/main/java/com/example/locale/activities/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/locale/activities/ForgotPasswordActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,7 +17,7 @@ import com.parse.RequestPasswordResetCallback;
 public class ForgotPasswordActivity extends AppCompatActivity {
     Button mBtnSubmit;
     TextView mEmail;
-
+    TextView mResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,21 +29,38 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         actionBar.hide();
 
         mEmail = findViewById(R.id.etEmailFP);
+        mResponse = findViewById(R.id.tvResponse);
 
         mBtnSubmit = findViewById(R.id.btnForgotPassword);
         mBtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ParseUser.requestPasswordResetInBackground(mEmail.getText().toString(), new RequestPasswordResetCallback() {
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            // An email was successfully sent with reset instructions.
-                        } else {
-                            // Something went wrong. Look at the ParseException to see what's up.
+                String userEmail = mEmail.getText().toString();
+                if (!isEmailValid(userEmail)){
+                    mResponse.setText("Please enter a valid email address.");
+                    mResponse.setVisibility(View.VISIBLE);
+                }
+                else {
+                    ParseUser.requestPasswordResetInBackground(userEmail, new RequestPasswordResetCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // An email was successfully sent with reset instructions.
+                                mResponse.setText("An email was successfully sent with reset instructions!");
+                                mResponse.setVisibility(View.VISIBLE);
+                            } else {
+                                // Something went wrong. Look at the ParseException to see what's up.
+                                mResponse.setText("Something went wrong.");
+                                mResponse.setVisibility(View.VISIBLE);
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
         });
     }
+
+    public boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
 }
